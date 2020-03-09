258dad67c2f8bc99a703f5683e2f5142a874dfdb
Evgeny <Kyberbote@gmail.com>
2020-03-09 18:23:58 +0300

    Added tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,41 @@
 #include "gtest/gtest.h"
+TEST(lab6, task1) {
+	Cleaner worker = Cleaner(1, "Alex", 100);
+	worker.setWorktime(160);
+	worker.calculateSalary();
+	unsigned int salary = worker.getPayment();
+	EXPECT_EQ(salary, 16000);
+}
+
+TEST(lab6, task2) {
+	Project* project1 = new Project("project1", 50000);
+	Programmer worker = Programmer(0, "Alex", 150, 0.2);
+	worker.setProject_pointer(project1);
+	worker.setWorktime(160);
+	worker.calculateSalary();
+	unsigned int salary = worker.getPayment();
+	EXPECT_EQ(salary, 34000);
+}
+
+TEST(lab6, task3) {
+	Project* project1 = new Project("project1", 50000);
+	TeamLeader worker = TeamLeader(0, "Alex", 150, 0.2, 1000);
+	worker.setWorktime(180);
+	worker.setSubordinates(5);
+	worker.setProject_pointer(project1);
+	worker.calculateSalary();
+	unsigned int salary = worker.getPayment();
+	EXPECT_EQ(salary, 42000);
+}
+
+TEST(lab6, task4) {
+	Project* project1 = new Project("project1", 50000);
+	Project* project2 = new Project("project2", 70000);
+	SeniorManager worker = SeniorManager(0, "Alex", 0.2, 1000);
+	worker.setProject_pointer(project1);
+	worker.setProject_pointer(project2);
+	worker.setSubordinates(4);
+	worker.calculateSalary();
+	unsigned int salary = worker.getPayment();
+	EXPECT_EQ(salary, 28000);
+}
